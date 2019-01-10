59c11968ac2f2f8b405a0a97b9124df8dbcea2b9
Chih-Yu <lcd010308@gmail.com>
2019-01-10 23:52:02 +0800

    Move get possible actions function to board

--- src/agent.h
+++ src/agent.h
@@ -18,10 +18,10 @@ public:
     virtual bool check_for_win(const Board& b) { return b.game_over(); }
 };
 
-class Random_agent : public Agent {
+class RandomAgent : public Agent {
 public:
-    Random_agent() : Agent(), dis(0.0, 1.0) { engine.seed(11); }
-    virtual ~Random_agent() {}
+    RandomAgent() : Agent(), dis(0.0, 1.0) { engine.seed(11); }
+    virtual ~RandomAgent() {}
 
 protected:
     std::default_random_engine engine;
@@ -32,9 +32,9 @@ protected:
  * dummy player
  * select a legal random action
  */
-class Player : public Random_agent {
+class Player : public RandomAgent {
 public:
-    Player(unsigned color, Tuple *tuple) : Random_agent(), color(color), tuple(tuple) {}
+    Player(unsigned color, Tuple *tuple) : RandomAgent(), color(color), tuple(tuple) {}
     std::string role() { return color ? "White" : "Black"; }
 
     virtual void open_episode() {
@@ -51,68 +51,20 @@ public:
         if (color == 0 && white_bitcount == 0) result = 1.0f;
         else if (color == 1 && black_bitcount == 0) result = 1.0f;
         else    result = -1.0f;
-        
+
         for (int i = record.size() - 1; i >= 0; i--)
             tuple->train_weight(record[i], result);
     }
 
 public:
     virtual Action take_action(const Board& before) {
-        
-        Board::data mine = before.get_board(color);
-        Board::data theirs = before.get_board(color ^ 1);
-        Board::data occupied = mine | theirs | Board::BORDER;
-        Board::data empty = ~occupied;
+        std::vector<unsigned> moves;
+        std::vector<unsigned> eats;
+        before.get_possible_eat(eats, color);
+        before.get_possible_move(moves, color);
 
-        eats.clear();
-        moves.clear();
-
-        // eatable
-        for (auto &cc : CIRCLE) {
-            unsigned cc_cir[4][2] = {{ 0 }};
-            // choose the eater position (initial section in 1 of 4)
-            for (auto *i = &cc[21]; i != &cc[24]; i++) {
-                if (empty & (1ULL << *i)) continue;
-                // which square are eater and it's belonging (add the 7th bit, 1 mine or 0 theirs)
-                cc_cir[3][0] = *i | ((mine & (1ULL << *i)) >> (*i - 7));
-            }
-            for (unsigned j = 0; j < 4; j++) {
-                unsigned &old_eater = cc_cir[(j + 3) & 3][0]; // same as cc_cir[(j-1+4) % 4][0]
-                unsigned &eatee = cc_cir[j][1];
-                unsigned &eater = cc_cir[j][0];
-                for (auto *i = &cc[j * 6]; i != &cc[(j + 1) * 6]; i++) {
-                    if (empty & (1ULL << *i)) continue;
-                    eater = *i | ((mine & (1ULL << *i)) >> (*i - 7));
-                    // check the eatee and take the square at cross into consideration
-                    if (eatee == 0 && *i != (old_eater & 0b111111))
-                        eatee = eater;
-                }
-            }
-            // select eater
-            for (unsigned i = 0; i < 4; i++) {
-                if ((cc_cir[i][0] & (1 << 7)) == 0) continue; // only check if mine piece is eater
-                // select eatee
-                for (unsigned j = 1; j < 4; j++) {
-                    unsigned row = (i + j) & 3;
-                    if(cc_cir[row][1] == 0) continue;     // the line is consider empty
-                    if(cc_cir[row][1] & (1 << 7)) break;  // the eatee is mine
-                    unsigned code = (cc_cir[i][0] & 0b111111) | ((cc_cir[row][1] & 0b111111) << 6);
-                    eats.push_back(code);
-                    break;
-                }
-            }
-        }
-
-        // movable
-        for (unsigned i = 9; i < 55; i++) {
-            if (!(mine & (1ULL << i)))   continue;
-            for (const unsigned &j : NEIGHBOR) {
-                if (empty & (1ULL << (i - j))) moves.push_back(((i - j) << 6) | i);
-                if (empty & (1ULL << (i + j))) moves.push_back(((i + j) << 6) | i);
-            }
-        }
-
-        if (/*color == 0 &&*/ dis(engine) < epsilon) {
+        // exploitation
+        if (dis(engine) < epsilon) {
             float best_value = -1e9;
             unsigned best_code = 0;
             Board best_state;
@@ -146,7 +98,8 @@ public:
                 else                    return Action::Move(best_code);
             }       
         }
-        else/* if (color == 0)*/ {
+        // exploration
+        else {
             Board tmp = Board(before);
             std::shuffle(eats.begin(), eats.end(), engine);
             std::shuffle(moves.begin(), moves.end(), engine);
@@ -165,23 +118,9 @@ public:
                     record.emplace_back(tmp);
                     return Action::Move(moves[0]);
                 }
-            }       
+            }
         }
-        /*else {
-            std::shuffle(eats.begin(), eats.end(), engine);
-            std::shuffle(moves.begin(), moves.end(), engine);
 
-            int size1 = eats.size(), size2 = moves.size();
-            if (dis(engine) * (size1 + size2) < size1) {
-                if (eats.size() > 0)   return Action::Eat(eats[0]);
-            }
-            else {
-                if (moves.size() > 0)   return Action::Move(moves[0]);
-            }
-            
-            // if (eats.size() > 0)     return Action::Eat(eats[0]);
-            // if (moves.size() > 0)    return Action::Move(moves[0]);
-        }*/
         return Action();
     }
 
@@ -190,8 +129,6 @@ private:
 
 private:
     unsigned color; // 0 for black or 1 for white
-    std::vector<unsigned> moves;
-    std::vector<unsigned> eats;
     Tuple *tuple;
     float epsilon;
 };
--- src/board.h
+++ src/board.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <array>
 #include <iostream>
+#include <vector>
+#include "utilities.h"
 #define F_LAYER 0x0055005500550055ULL
 #define S_LAYER 0x0000333300003333ULL
 #define T_LAYER 0x000000000F0F0F0FULL
@@ -26,6 +28,8 @@ public:
 
 public:
     Board() : board_white(0x007E7E0000000000ULL), board_black(0x00000000007E7E00ULL) {}
+    Board(const Board& b) = default;
+    Board& operator =(const Board& b) = default;
     data& get_board(unsigned int i) {
         return (i) ? board_white : board_black;
     }
@@ -35,6 +39,66 @@ public:
 
     const bool game_over() const {
         return !board_white || !board_black;
+    }
+
+public:
+    void get_possible_eat(std::vector<unsigned> &eats, int color) const {
+        Board::data mine = color ? board_white : board_black;
+        Board::data theirs = (color ^ 1) ? board_white : board_black;
+        Board::data occupied = mine | theirs | Board::BORDER;
+        Board::data empty = ~occupied;
+
+        eats.clear();
+        for (auto &cc : CIRCLE) {
+            unsigned cc_cir[4][2] = {{ 0 }};
+            // choose the eater position (initial section in 1 of 4)
+            for (auto *i = &cc[21]; i != &cc[24]; i++) {
+                if (empty & (1ULL << *i)) continue;
+                // which square are eater and it's belonging (add the 7th bit, 1 mine or 0 theirs)
+                cc_cir[3][0] = *i | ((mine & (1ULL << *i)) >> (*i - 7));
+            }
+            for (unsigned j = 0; j < 4; j++) {
+                unsigned &old_eater = cc_cir[(j + 3) & 3][0]; // same as cc_cir[(j-1+4) % 4][0]
+                unsigned &eatee = cc_cir[j][1];
+                unsigned &eater = cc_cir[j][0];
+                for (auto *i = &cc[j * 6]; i != &cc[(j + 1) * 6]; i++) {
+                    if (empty & (1ULL << *i)) continue;
+                    eater = *i | ((mine & (1ULL << *i)) >> (*i - 7));
+                    // check the eatee and take the square at cross into consideration
+                    if (eatee == 0 && *i != (old_eater & 0b111111))
+                        eatee = eater;
+                }
+            }
+            // select eater
+            for (unsigned i = 0; i < 4; i++) {
+                if ((cc_cir[i][0] & (1 << 7)) == 0) continue; // only check if mine piece is eater
+                // select eatee
+                for (unsigned j = 1; j < 4; j++) {
+                    unsigned row = (i + j) & 3;
+                    if(cc_cir[row][1] == 0) continue;     // the line is consider empty
+                    if(cc_cir[row][1] & (1 << 7)) break;  // the eatee is mine
+                    unsigned code = (cc_cir[i][0] & 0b111111) | ((cc_cir[row][1] & 0b111111) << 6);
+                    eats.push_back(code);
+                    break;
+                }
+            }
+        }
+    }
+
+    void get_possible_move(std::vector<unsigned> &moves, int color) const {
+        Board::data mine = color ? board_white : board_black;
+        Board::data theirs = (color ^ 1) ? board_white : board_black;
+        Board::data occupied = mine | theirs | Board::BORDER;
+        Board::data empty = ~occupied;
+
+        moves.clear();
+        for (unsigned i = 9; i < 55; i++) {
+            if (!(mine & (1ULL << i)))   continue;
+            for (const unsigned &j : NEIGHBOR) {
+                if (empty & (1ULL << (i - j))) moves.push_back(((i - j) << 6) | i);
+                if (empty & (1ULL << (i + j))) moves.push_back(((i + j) << 6) | i);
+            }
+        }
     }
 
 public:
